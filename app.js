94aa9b5b88a3dda041c8160eda83c8bc1076f669
your_username <dannyclassic56@gmail.com>
2022-12-31 22:04:32 +0100

    Happy New Year

---
+++ app.js
@@ -0,0 +1,15 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+  const audio = document.querySelector("audio");
+  audio.volume = 0.2;
+  audio.play();
+});
+
+const spans = document.querySelectorAll("span");
+
+spans.forEach((span) => {
+  span.addEventListener("click", function () {
+    let msg = new SpeechSynthesisUtterance();
+    msg.text = `Happy New Year Guys, hope you are ready for this year's rollercoster cuz I am!`;
+    window.speechSynthesis.speak(msg);
+  });
+});
